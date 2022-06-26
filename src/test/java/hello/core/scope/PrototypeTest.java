c11eccd612f3a3af5fde1f6c351ce1e6ae199bef
tmxhsk99 <tmxhsk99@naver.com>
2022-06-27 05:57:44 +0900

    feat: 프로토/싱글톤 타입 빈 테스트 추가

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,35 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+    @Test
+    public void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 : " + prototypeBean1);
+        System.out.println("prototypeBean2 : " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        ac.close();
+    }
+    //component 가 없는 이유는 AnnotationConfigApplicationContext를 호출하면 알아서 빈객체처럼 취급해줌
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
